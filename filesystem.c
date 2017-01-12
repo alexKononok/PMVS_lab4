4e07362ebb72ddef7cfae091b1362ab6ddb6b0fe
alexKononok <alex.kononok@gmail.com>
2017-01-12 14:27:13 +0300

    add getattr: The getattr callback is in charge of reading the metadata of a given path

--- filesystem.c
+++ filesystem.c
@@ -32,4 +32,25 @@ static int path_index(const char* path)
 	return -1;
 }
 
-
+static int getattr_callback(const char *path, struct stat *stbuf) 
+{
+	memset(stbuf, 0, sizeof(struct stat));
+	if (strcmp(path, "/") == 0) {
+		stbuf->st_mode = S_IFDIR | 0755;
+		stbuf->st_nlink = 2;
+		return 0;
+	} else {
+		int index = path_index(path);
+		if(index == -1) {
+			return -ENOENT;
+		}
+		stbuf->st_mode = S_IFREG | 0777;
+		stbuf->st_nlink = 1;
+		int start = index == 0 ? 0 : file_offset_end[index-1];
+		int size = file_offset_end[index]-start;
+		printf("%d\n", size);
+		stbuf->st_size = file_size[index];
+		return 0;
+	}
+	return -ENOENT;
+}
